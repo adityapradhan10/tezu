b5ba27d985477e17a8639c699e33734c96499407
Aditya Pradhan <contact.adityapradhan.me@gmail.com>
2025-03-22 19:18:34 +0530

    TEZU-1: Setup project with turborepo

---
+++ apps/playground/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1>This is the playground app!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ packages/ui/eslint.config.mjs
@@ -0,0 +1,4 @@
+import { config } from "@tezu/eslint-config/react-internal";
+
+/** @type {import("eslint").Linter.Config} */
+export default config;
