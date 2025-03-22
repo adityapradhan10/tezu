6ea8601d0c0d65a995747d2bc012b9f45f75890d
Aditya Pradhan <contact.adityapradhan.me@gmail.com>
2025-03-22 19:22:39 +0530

    TEZU-2: Consumed button component in the playground app

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@tezu/ui/button";
+
 const App = () => {
   return (
     <div>
       <h1>This is the playground app!</h1>
+      <Button appName="playground">
+        Open alert
+      </Button>
     </div>
   );
 };
